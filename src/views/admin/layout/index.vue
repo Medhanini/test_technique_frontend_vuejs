9da9fadb3560a7c5df4b1487f73615360a3da66c
simo <mohamed.hanini95@gmail.com>
2022-07-22 12:00:11 +0100

    Modify custom the admin layout

--- src/views/admin/layout/index.vue
+++ src/views/admin/layout/index.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-      
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Start of Topbar -->
+        <Topbar />
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+          <router-view></router-view>
+        </div>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; test technique vuejs 2022 By Mohamed hanini</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+    </div>
+    <!-- End of Content Wrapper -->
   </div>
 </template>
 
 <script>
+import Topbar from "../components/Topbar";
+
 export default {
-
-}
+  name: "AdminLayout",
+  components: {
+    Topbar,
+  }
+};
 </script>
-
-<style>
-
-</style>
