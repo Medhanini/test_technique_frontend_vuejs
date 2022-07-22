52a4f1031f2f57463f03cec96e5ec48b0b6104a8
simo <mohamed.hanini95@gmail.com>
2022-07-22 13:27:26 +0100

    Modify secure routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AdminLayout from "../views/admin/layout/index";
+import store from '../store'
 Vue.use(VueRouter)
 
 const routes = [
+
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/login',
     name: 'login',
-    component: () => import('../views/Login/index.vue')
+    component: () => import('../views/Login/index.vue'),
+    meta: { guest : true }
   },
   {
     path: '/register',
     name: 'register',
-    component: () => import('../views/RegisterView')
+    component: () => import('../views/RegisterView'),
+    meta: { guest : true }
   },
   /**
     * Admin routes
@@ -36,7 +26,8 @@ const routes = [
   name: "admin",
   component: () => import("../views/admin/DashboardView.vue"),
   meta: {
-      layout: AdminLayout
+      layout: AdminLayout,
+      secure : true
   }
   },
   {
@@ -44,7 +35,8 @@ const routes = [
     name: "profile",
     component: () => import("../views/admin/ProfileView.vue"),
     meta: {
-        layout: AdminLayout
+        layout: AdminLayout,
+        secure : true
     }
   },
 ]
@@ -53,6 +45,32 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.secure)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (store.state.auth.loggedIn == false) {
+      next({
+        path: '/login'
+      })
+    } else {
+      next()
+    }
+  } 
+  else if(to.matched.some(record => record.meta.guest)) {
+      if (store.state.auth.loggedIn == false) {
+          next()
+        } else {
+          next({
+              path: '/'
+            })
+        } 
+    }
+  else {
+    next() // make sure to always call next()!
+  }
 })
 
 export default router
