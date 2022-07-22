c164daee6f1b66beeeb3f1cb365504555c291b51
simo <mohamed.hanini95@gmail.com>
2022-07-22 15:35:27 +0100

    Modify Fix bugs

--- src/views/admin/components/TopBar.vue
+++ src/views/admin/components/TopBar.vue
@@ -1,5 +1,5 @@
 <template>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="container navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -20,7 +20,7 @@
                   </a>
                     <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
                       <li>
-                          <router-link class="dropdown-item" to="/about">
+                          <router-link class="dropdown-item" to="/admin/profile">
                               Profile
                           </router-link></li>
                       <li>
