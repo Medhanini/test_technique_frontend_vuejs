2b54edd88214352635db2a24fc128ed981dd821e
simo <mohamed.hanini95@gmail.com>
2022-07-22 11:55:44 +0100

    Modify auto changement of layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <template>
-  <div>
+  <div id="app">
     <notifications group="foo" />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
     <component :is="this.$route.meta.layout || 'div'">
       <router-view/>
     </component>
   </div>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
