c4a2375a29e1dacebcd8b0c573b586c43626f22e
simo <mohamed.hanini95@gmail.com>
2022-07-22 13:12:47 +0100

    Modify TheChartLine component

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -250,7 +250,7 @@
               justify-content-between
             "
           >
-            <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Frameworks </h6>
             <div class="dropdown no-arrow">
               <a
                 class="dropdown-toggle"
@@ -285,6 +285,58 @@
           </div>
         </div>
       </div>
+
+      <!-- Area Chart -->
+      <div class="col-xl-8 col-lg-7">
+        <div class="card shadow mb-4">
+          <!-- Card Header - Dropdown -->
+          <div
+            class="
+              card-header
+              py-3
+              d-flex
+              flex-row
+              align-items-center
+              justify-content-between
+            "
+          >
+            <h6 class="m-0 font-weight-bold text-primary">Lines Overview</h6>
+            <div class="dropdown no-arrow">
+              <a
+                class="dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+              </a>
+              <div
+                class="
+                  dropdown-menu dropdown-menu-right
+                  shadow
+                  animated--fade-in
+                "
+                aria-labelledby="dropdownMenuLink"
+              >
+                <div class="dropdown-header">Dropdown Header:</div>
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
+          </div>
+          <!-- Card Body -->
+          <div class="card-body">
+              <TheChartLine />
+          </div>
+        </div>
+      </div>
+
+
     </div>
 
     <!-- Content Row -->
@@ -494,6 +546,7 @@ export default {
       TheChartBar:() => import('./components/TheChartBar.vue'),
       TheChartBubble:() => import('./components/TheChartBubble.vue'),
       TheChartDoughnut:() => import('./components/TheChartDoughnut.vue'),
+      TheChartLine:() => import('./components/TheChartLine.vue'),
     },
 };
 </script>
