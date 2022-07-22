f9f18cf04b5389b3dd0271a4a36218fa51aa8f22
simo <mohamed.hanini95@gmail.com>
2022-07-22 13:20:18 +0100

    Install  axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+
+Vue.use(VueAxios, axios);
+
 import 'bootstrap';
 import './assets/app.scss';
 import Notifications from 'vue-notification'
