d3b5b2f7cd27d377036eeb373ec642c86793dacc
simo <mohamed.hanini95@gmail.com>
2022-07-22 03:43:12 +0100

    Add Admin views routes

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,97 @@ const routes = [
     path: '/register',
     name: 'register',
     component: () => import('../views/RegisterView')
+  },
+   /**
+         * Admin routes
+         */
+    {
+      path: "/admin",
+      name: "admin",
+      component: () => import("./views/admin/dashboard.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/components/buttons",
+      name: "buttons",
+      component: () => import("./views/admin/buttons.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/components/cards",
+      name: "cards",
+      component: () => import("./views/admin/cards.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/utilities/colors",
+      name: "colors",
+      component: () => import("./views/admin/colors.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/utilities/borders",
+      name: "borders",
+      component: () => import("./views/admin/borders.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/utilities/animations",
+      name: "animations",
+      component: () => import("./views/admin/animations.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/utilities/other",
+      name: "other",
+      component: () => import("./views/admin/other.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/pages/page-not-found",
+      name: "page-not-found",
+      component: () => import("./views/admin/page-not-found.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/pages/blank",
+      name: "blank",
+      component: () => import("./views/admin/blank.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/charts",
+      name: "charts",
+      component: () => import("./views/admin/charts.vue"),
+      meta: {
+        layout: AdminLayout
+      }
+  },
+  {
+      path: "/admin/tables",
+      name: "tables",
+      component: () => import("./views/admin/tables.vue"),
+      meta: {
+        layout: AdminLayout
+      }
   }
 ]
 
